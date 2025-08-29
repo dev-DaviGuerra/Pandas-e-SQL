fd1bb6cdebefbcb9dae5a39f60ded640487c761f
dev-DaviGuerra <davimguerra@gmail.com>
2025-08-29 11:28:37 -0300

    CREATE and DROP

--- SQL/sql.py
+++ SQL/sql.py
@@ -6,3 +6,12 @@ conn = sqlite3.connect('web.db')
 df_data = pd.read_csv('bd_data.csv', index_col=0)
 df_data.index.name = 'index_name'
 df_data.to_sql('data', conn, index_label='index_name')
+
+c = conn.cursor()
+c.execute('CREATE TABLE products (product_id, product_name, price)')
+conn.commit()
+
+c.execute('DROP TABLE products')
+c.execute('DROP TABLE data')
+
+c.execute('CREATE TABLE products ([product_id] INTEREGER PRIMARY KEY, [product_name] TEXT, [price] INTEREGER)')
