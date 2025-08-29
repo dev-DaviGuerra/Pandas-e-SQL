34f04ad0534f9fb1c029532533ae8e7a75cc33bc
dev-DaviGuerra <davimguerra@gmail.com>
2025-08-29 12:13:24 -0300

    INSERT

--- SQL/sql.py
+++ SQL/sql.py
@@ -10,8 +10,19 @@ df_data.to_sql('data', conn, index_label='index_name')
 c = conn.cursor()
 c.execute('CREATE TABLE products (product_id, product_name, price)')
 conn.commit()
-
 c.execute('DROP TABLE products')
 c.execute('DROP TABLE data')
-
 c.execute('CREATE TABLE products ([product_id] INTEREGER PRIMARY KEY, [product_name] TEXT, [price] INTEREGER)')
+
+
+c.execute('''INSERT INTO products (product_id, product_name, price)
+    VALUES
+    (1, 'Computer', 800),
+    (2, 'Telefone', 500),
+    (3, 'Teclado', 300)
+
+''')
+conn.commit()
+
+df_data2 = df_data.iloc[::-2]
+df_data2.to_sql('data', conn, if_exists='append')
